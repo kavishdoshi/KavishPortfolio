8a548a09591218c0545687c2c7848cf2a3e471a8
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-08 12:47:11 -0700

    skill update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,8 @@ import {motion} from 'framer-motion'
 
 const Skill = ({name, x, y}) => {
     return (
-        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:bg-light dark:text-dark lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transperent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:text-bold lg:p-6 md:p-4 xs:text-xs xs:p-2' 
+        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:bg-light dark:text-dark 
+        lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:text-bold lg:p-6 md:p-4 xs:text-xs xs:p-2' 
         whileHover={{scale:1.05}} 
         initial={{x:0, y:0}} 
         whileInView= {{x:x, y:y, transition: {duration:1.5}}}
@@ -29,7 +30,7 @@ const Skills = () => {
                 <Skill name="C#" x="-20vw" y="-15vw" />
                 <Skill name="Python" x="15vw" y="-12vw" />
                 <Skill name="Java" x="32vw" y="-5vw" />
-                <Skill name="Git" x="0vw" y="-20vw" />
+                <Skill name="Git" x="-5vw" y="-30vw" />
                 <Skill name="Docker" x="-25vw" y="18vw" />
                 <Skill name="AWS" x="-25vw" y="-6vw" />
                 <Skill name="Microsoft Azure" x="15vw" y="-20vw" />
