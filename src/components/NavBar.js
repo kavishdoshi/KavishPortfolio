f3933b7f0a52e83ba49b64c9b31dbb3186350f32
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-08 13:15:14 -0700

    dark-light

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -86,7 +86,8 @@ const NavBar = () => {
         </div>
         {
             isOpen ? 
-                <motion.div initial={{scale:0, opacity:0, x: "-50%", y:"-50%"}} animate={{scale: 1, opacity:1}} className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
+                <motion.div initial={{scale:0, opacity:0, x: "-50%", y:"-50%"}} animate={{scale: 1, opacity:1}} 
+                    className='min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
                     <nav className='flex items-center flex-col justify-center'>
                         <CustomMobileLink href="/" title="Home" className='' toggle = {handleClick}/>
                         <CustomMobileLink href="/about" title="About" className=''toggle = {handleClick}/>
@@ -110,7 +111,7 @@ const NavBar = () => {
                             <DribbbleIcon />
                         </motion.a>
 
-                        <button onClick={() => setMode(mode === "light" ? "dark" : "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 sm:mx-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
+                        <button onClick={() => setMode(mode === "light" ? "dark" : "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
                             {
                                 mode === "dark" ? <SunIcon className= {"fill-dark"} /> : <MoonIcon className={"fill-dark"} />
                             }
