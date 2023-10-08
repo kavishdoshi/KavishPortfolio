dec72e0421e281bd0d6e263dbc4e1f23019d4416
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-08 14:00:06 -0700

    keep on trying

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,9 @@ export default function Home() {
               <AnimatedText text = "Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left xl:!text-5xl lg:!left-center lg:!text=6xl md:!text-5xl sm:!text-3xl'/>
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>As a skilled developer, I am dedicated to turning ideas into applications. </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="/KavishDoshiResume.pdf" target={"_blank"} className='md:p-2 md:px-4 md:text-base flex items-center bg-dark text-light p-2.5 px-6 rounded-lg lext-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:text-dark dark:bg-light hover:dark:bg-dark hover:dark:border-light hover:dark:text-light' download={true}>
-                  Resume <LinkArrow className={"w-6 ml-1"} />
+                <Link href="/KavishDoshiResume.pdf" target={"_blank"} 
+                      className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg lext-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:text-dark dark:bg-light hover:dark:bg-dark hover:dark:border-light hover:dark:text-light md:p-2 md:px-4 md:text-base' download={true}>
+                  Resume <LinkArrow className={"w-6 ml-1 md:w-3"} />
                 </Link>
                 <Link href="mailto:kavishdoshi28@gmail.com" target={"_blank"}
                 className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base' 
