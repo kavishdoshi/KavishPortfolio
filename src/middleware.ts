5a9f0be4ccbb11ed9b51d1e9f3e97ba6c77e61af
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-22 15:53:46 -0700

    cookie

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+
+    const response = NextResponse.next({ 
+        request : { headers : req.headers, },
+    })
+
+    if (req.cookies.has('x-custom-auth-header') && req.cookies.get('x-custom-auth-header').value == 'isAuthed')
+    {
+        return NextResponse.next();
+    }
+
+    response.cookies.set("x-redirect-url", req.url); 
+    return NextResponse.redirect("http://login.kavishdoshi.com/signup");
+}
+
+export const config = {
+    matcher: ['/', '/about', '/projects'],
+}
