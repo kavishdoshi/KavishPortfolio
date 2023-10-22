10f0fa129c9f71ea0b028ebb42c6352898d1dbef
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-22 16:23:04 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx'],
+  pageExtensions: ['ts', 'tsx', 'js'],
 }
 
 module.exports = nextConfig
