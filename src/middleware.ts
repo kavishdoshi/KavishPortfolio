91e535c838901e7478d9ce48a6a1c685f9a9af79
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-22 17:10:45 -0700

    headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,19 @@ export function middleware(req: NextRequest) {
         request : { headers : req.headers, },
     })
 
+    if(req.headers.has('x-custom-auth-header') && req.headers.get('x-custom-auth-header') == 'isAuthed')
+    {
+        return NextResponse.next();
+    }
+
     if (req.cookies.has('x-custom-auth-header') && req.cookies.get('x-custom-auth-header').value == 'isAuthed')
     {
         return NextResponse.next();
     }
 
     response.cookies.set("x-redirect-url", req.url); 
-    return NextResponse.redirect("https://login.kavishdoshi.com/signup");
+    response.headers.set("x-redirect-url", req.url);
+    return NextResponse.redirect("https://login.kavishdoshi.com/signup", { headers: response.headers });
 }
 
 export const config = {
