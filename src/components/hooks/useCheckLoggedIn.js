63fefdc9733ab557212b8f681b610308ba0f844d
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-23 02:25:00 -0700

    loggedin

--- src/components/hooks/useCheckLoggedIn.js
+++ src/components/hooks/useCheckLoggedIn.js
@@ -1,13 +1,23 @@
 import Cookie from 'js-cookie'
 import { useEffect } from 'react'
 import checkLoggedIn from '@/pages/api/checkLoggedIn';
+import { getServerSession } from "next-auth/next";
 
 const CheckLoggedIn = ({route}) => {
     useEffect( async () => {
-        const response = await fetch(checkLoggedIn, {
-            method : 'POST',
-          });
-        const loggedIn = await response.text();
+        // const session = await getServerSession();
+        // const response = await fetch(checkLoggedIn, {
+        //     method : 'POST',
+
+        //   });
+        const loggedIn = false;
+        if (Cookie.get('Auth'))
+        {
+            if (Cookie.get('Auth') == 'Allowed')
+            {
+                loggedIn = "true"
+            }
+        }
         if (loggedIn == "false")
         {
             Cookie.set('redirectURL', route, { expires: 1 , domain: '.kavishdoshi.com'})
