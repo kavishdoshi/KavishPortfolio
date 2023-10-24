c5ff5fe719d43fe000111fedae2f4e859a11366f
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-23 22:42:04 -0700

    redirect

--- src/components/hooks/useCheckLoggedIn.js
+++ src/components/hooks/useCheckLoggedIn.js
@@ -2,29 +2,25 @@ import Cookie from 'js-cookie'
 import { useEffect } from 'react'
 import checkLoggedIn from '@/pages/api/checkLoggedIn';
 
-const checkLogin = async ({route}) => {
-    const response = await fetch(checkLoggedIn, {
-        method : 'POST',
-
-      });
-    const data = await response.text();
-    console.log(data);
-    let loggedIn = "false";
-    if (Cookie.get('Auth') == 'Allowed')
-    {
-        loggedIn = "true"
-    }
-    if (loggedIn == "false")
-    {
-        Cookie.set('redirectURL', route, { expires: 1 , domain: '.kavishdoshi.com'})
-        window.location.href = "https://login.kavishdoshi.com/login"
-    }
-}
-
 
 const CheckLoggedIn = ({route}) => {
     useEffect(async () => {
-        await checkLogin(route=route);
+        const response = await fetch(checkLoggedIn, {
+            method : 'POST',
+    
+          });
+        const data = await response.text();
+        console.log(data);
+        let loggedIn = "false";
+        if (Cookie.get('Auth') == 'Allowed')
+        {
+            loggedIn = "true"
+        }
+        if (loggedIn == "false")
+        {
+            Cookie.set('redirectURL', route, { expires: 1 , domain: '.kavishdoshi.com'})
+            window.location.href = "https://login.kavishdoshi.com/login"
+        }
     }, [])
 }
 
