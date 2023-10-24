915e61d1bf012eddcb046be016de5b67ae8b9daf
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-23 20:44:20 -0700

    api

--- src/components/hooks/useCheckLoggedIn.js
+++ src/components/hooks/useCheckLoggedIn.js
@@ -2,27 +2,33 @@ import Cookie from 'js-cookie'
 import { useEffect } from 'react'
 import checkLoggedIn from '@/pages/api/checkLoggedIn';
 
-const CheckLoggedIn = ({route}) => {
-    useEffect( async () => {
-        
-        const response = await fetch(checkLoggedIn, {
-            method : 'POST',
+const checkLogin = async ({route}) => {
+    const response = await fetch(checkLoggedIn, {
+        method : 'POST',
 
-          });
-        console.log(response.text())
-        let loggedIn = "false";
-        if (Cookie.get('Auth'))
+      });
+    const data = await response.text();
+    console.log(data);
+    let loggedIn = "true";
+    if (Cookie.get('Auth'))
+    {
+        if (Cookie.get('Auth') == 'Allowed')
         {
-            if (Cookie.get('Auth') == 'Allowed')
-            {
-                loggedIn = "true"
-            }
+            loggedIn = "true"
         }
-        if (loggedIn == "false")
-        {
-            Cookie.set('redirectURL', route, { expires: 1 , domain: '.kavishdoshi.com'})
-            window.location.href = "https://login.kavishdoshi.com/login"
-        }
+    }
+    if (loggedIn == "false")
+    {
+        Cookie.set('redirectURL', route, { expires: 1 , domain: '.kavishdoshi.com'})
+        window.location.href = "https://login.kavishdoshi.com/login"
+    }
+    return data;
+}
+
+
+const CheckLoggedIn = ({route}) => {
+    useEffect(async () => {
+    checkLogin(route);
     }, [])
 }
 
